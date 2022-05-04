9f3046a837434259e9a3813f4ae7d6909e804cb3
koramga <koramga@gmail.com>
2022-05-04 18:46:15 +0900

    Asra Project 시작!

---
+++ Source/Asra/Character/AsraCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsraCharacter.h"
+
+
+// Sets default values
+AAsraCharacter::AAsraCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAsraCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAsraCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AAsraCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Asra/Game/GameMode/AsraGameMode.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsraGameMode.generated.h"
+
+UCLASS()
+class ASRA_API AAsraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAsraGameMode();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
